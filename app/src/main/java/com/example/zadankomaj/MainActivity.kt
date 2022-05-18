7b5e5664dbfcb9b229d60f094b759b557c9e51dd
PatrykD2pr <patrykdomaszek@gazeta.pl>
2022-05-18 07:57:52 +0200

    zmiana obrazkow dziala

--- app/src/main/java/com/example/zadankomaj/MainActivity.kt
+++ app/src/main/java/com/example/zadankomaj/MainActivity.kt
@@ -3,14 +3,44 @@ package com.example.zadankomaj
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var zdj = 1
-        findViewById<Button>(R.id.button).setOnClickListener {
-            if(zdj==1)
+        var x = 0;
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            x--
+            if (x == 1) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z1)
+            } else if (x == 2) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z2)
+            } else if (x == 3) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z3)
+            } else if (x == 4) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z4)
+            } else if (x == 0) {
+                x = 1
+            }
         }
+        findViewById<Button>(R.id.button).setOnClickListener {
+            x++
+            if (x == 1) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z1)
+            } else if (x == 2) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z2)
+            } else if (x == 3) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z3)
+            } else if (x == 4) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z4)
+            }
+            else if (x == 5) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.z5)
+            } else if (x == 6)
+                x = 5
+        }
+        findViewById<CheckBox>(
     }
 }
